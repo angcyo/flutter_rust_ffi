9ba10d6e4aa7e3aeaa522030a6114fffba5115df
angcyo <angcyo@126.com>
2024-08-17 14:44:34 +0800

    init

---
+++ example/android/app/src/main/kotlin/com/angcyo/ffi/flutter_rust_ffi_example/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.angcyo.ffi.flutter_rust_ffi_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ macos/Classes/flutter_rust_ffi.c
@@ -0,0 +1,3 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../flutter_rust_ffi.podspec for more information.
+#include "../../src/flutter_rust_ffi.c"
